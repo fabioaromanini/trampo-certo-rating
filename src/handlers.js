f5af0b7ecbaf3adf6a8a9119c81f619674d25b9b
Fabio Romanini <fabioaromanini@gmail.com>
2020-01-22 15:53:47 -0200

    creates boilerplate for setRatings function

--- src/handlers.js
+++ src/handlers.js
@@ -29,4 +29,31 @@ module.exports = {
       };
     }
   },
+  setRating: async event => {
+    // let body, statusCode;
+    // try {
+    //   const { cpf } = event.queryStringParameters;
+    //   const ratingByCpf = await ratingController.getRatingByCpf(cpf);
+
+    //   statusCode = 200;
+    //   body = JSON.stringify(ratingByCpf);
+    // } catch (e) {
+    //   if (!event.queryStringParameters || e.message === '400') {
+    //     statusCode = 400;
+    //     body = 'Missing CPF in query string';
+    //   } else {
+    //     statusCode = 500;
+    //   }
+    // } finally {
+    //   return {
+    //     statusCode,
+    //     body,
+    //     headers: corsHeaders,
+    //   };
+    // }
+    return {
+      statusCode: 200,
+      headers: corsHeaders,
+    };
+  },
 };
